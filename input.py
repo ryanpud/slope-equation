860ff27932dbf95dcef044df3686cdfe63710482
ryanpud <rpudliner22@gmail.com>
2022-02-10 13:08:59 -0500

    This is the first commit, I began creating a way to pull numbers out of the ordered pairs and am planning on creating a test now

---
+++ input.py
@@ -0,0 +1,11 @@
+pair1 = input('Please Enter the First Ordered Pair:')
+pair2 = input('Please Enter the Second Ordered Pair:')
+try:
+    x1 = int(pair1[1])
+    y1 = int(pair1[3])
+    x2 = int(pair2[1])
+    y2 = int(pair2[3])
+except:
+        quit()
+
+
