d2dcd1d3ca4f59fee0050e4d2436e98c26f75234
ryanpud <rpudliner22@gmail.com>
2022-02-11 08:51:04 -0500

    Changed the input to 4 instead of 2

--- run/input.py
+++ run/input.py
@@ -1,11 +1,7 @@
-pair1 = input('Please Enter the First Ordered Pair:')
-pair2 = input('Please Enter the Second Ordered Pair:')
-try:
-    x1 = int(pair1[1])
-    y1 = int(pair1[3])
-    x2 = int(pair2[1])
-    y2 = int(pair2[3])
-except:
-        quit()
+x1 = input('Please Enter Your First X-Value:')
+x2 = input('Please Enter Your Second X-Value:')
+y1 = input('Please Enter Your First Y-Value:')
+y2 = input('Please Enter Your Second Y-Value:')
+
 
 
